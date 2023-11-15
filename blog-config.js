75b1100beaf5e17d74c9526cb888a17df34cb601
전승원 <seungwon7308@gmail.com>
2023-11-15 20:49:20 +0900

    seungwon_dev_log init

--- blog-config.js
+++ blog-config.js
@@ -1,20 +1,13 @@
 module.exports = {
-  title: "hoodie",
-  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
-  author: "Hudi",
+  title: "Seungwon",
+  description: "안녕하세요 깊은 성장이란 좋은 학습 곡선에서 나오다는 생각을 가져 하나를 알더라도 깊게 알도록 노력하는 주니어 백엔드 개발자 전승원입니다.",
+  author: "Seungwon",
   siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
   links: {
     github: "https://github.com/devHudi",
     linkedIn: "https://linkedin.com",
-    facebook: "https://www.facebook.com",
-    twitter: "https://twitter.com",
-    x: "https://x.com",
-    instagram: "https://www.instagram.com",
-    medium: "https://medium.com",
-    blogger: "https://blogger.com",
     email: "mailto:devhudi@gmail.com",
     resume: "https://hudi.blog",
-    link: "https://hudi.blog",
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
