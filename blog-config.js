337bbaa5a194e93a513eb7c166da4945c5b15b25
전승원 <seungwon7308@gmail.com>
2023-11-16 14:55:50 +0900

    :ambulance: :: 수정 사항

--- blog-config.js
+++ blog-config.js
@@ -2,12 +2,10 @@ module.exports = {
   title: "Seungwon",
   description: "안녕하세요 깊은 성장이란 좋은 학습 곡선에서 나오다는 생각을 가져 하나를 알더라도 깊게 알도록 노력하는 주니어 백엔드 개발자 전승원입니다.",
   author: "Seungwon",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  siteUrl: "https://quiet-squirrel-1544d9.netlify.app/",
   links: {
-    github: "https://github.com/devHudi",
-    linkedIn: "https://linkedin.com",
-    email: "mailto:devhudi@gmail.com",
-    resume: "https://hudi.blog",
+    github: "https://github.com/louis7308",
+    email: "mailto:seungwon7308@gmail.com",
   },
   utterances: {
     repo: "devHudi/gatsby-starter-hoodie",
